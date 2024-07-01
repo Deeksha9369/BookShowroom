d4e927da234cdd1996f73ff7c4bbb360b44cf0cc
Deeksha Shakya <your.email@example.com>
2024-07-01 10:55:17 +0530

    Frontend done!

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 function Cards({item}) {
   return (
     <>
-        <div className='mt-4 my-30 p-4 '>
-        <div className="card w-92 bg-base-100 shadow-xl hover:scale-150 duration-200 dark:bg-slate-900 dark:text-white dark:border  ">
-
-    <figure><img src={item.image} alt="Books" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      {item.name}
-      <div className="badge badge-secondary ">{item.category}</div>
-    </h2>
-    <p>{item.title}</p>
-    <div className="card-actions justify-between">
-      <div className="badge badge-outline">${item.price}</div> 
-      <div className=" cursor-pointer px-2 py-1 rounded-full border-[2p] hover:bg-blue-800 px-2 py-1 duration-200">By Now</div>
-    </div>
-  </div>
-</div>
+      <div className='mt-4 my-30 p-4 '>
+        <div className="card w-92 bg-base-100 shadow-xl hover:scale-110 duration-200 dark:bg-slate-900 dark:text-white dark:border "> 
+                  <figure><img src={item.image} alt="Books" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-secondary ">{item.category}</div>
+            </h2>
+            <p>{item.title}</p>
+            <div className="card-actions justify-between">
+              <div className="badge badge-outline">${item.price}</div>
+              <div className=" cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-blue-800 duration-200">Buy Now</div>
+            </div>
+          </div>
         </div>
+      </div>
     </>
   )
 }
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -34,25 +34,25 @@ useEffect(()=>{
     };
   },[]);
     const navItems=(
-    <>
+    < >
 
-      <li><a href="/">Home</a></li>
-      <li><a href="/course">Course</a></li>
-      <li><a href="/contact">Contact</a></li>
-      <li><a>About</a></li>
+      <li className='dark:bg-slate-900 dark:text-white'><a href="/">Home</a></li>
+      <li  className='dark:bg-slate-900 dark:text-white'><a href="/course">Course</a></li>
+      <li  className='dark:bg-slate-900 dark:text-white'><a href="/contact">Contact</a></li>
+      <li  className='dark:bg-slate-900 dark:text-white'><a>About</a></li>
       
     </>
     );
   return (
     <>
      <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 z-50 ${sticky ? "sticky-navbar shadow-md bg-base-200 duration-300 dark:bg-slate-900 dark:text-white transition-all ease-in-out" : ""}`}>
-     <div className="navbar">
+     <div className="navbar ">
   <div className="navbar-start">
-    <div className="dropdown">
+    <div className="dropdown ">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"> 
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52 dark:bg-slate-900 dark:text-white "> 
       {navItems} 
       </ul>
     </div>
